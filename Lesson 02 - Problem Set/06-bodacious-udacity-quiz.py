4bcd6976f1edb893abe2b02160be6d4ad6f09e3a
stevenxchung <stevenxchung@gmail.com>
2019-05-07 20:25:39 -0400

    Create 06-bodacious-udacity-quiz.py

---
+++ Lesson 02 - Problem Set/06-bodacious-udacity-quiz.py
@@ -0,0 +1,13 @@
+# Given the variables s and t defined as:
+
+
+s = 'udacity'
+t = 'bodacious'
+
+
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+
+print(s[0:1] + t[2:])
