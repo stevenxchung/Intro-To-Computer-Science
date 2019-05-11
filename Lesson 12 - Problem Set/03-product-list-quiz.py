ba0464b871844e466917f985e3d94e7056930de2
stevenxchung <stevenxchung@gmail.com>
2019-05-11 13:12:39 -0400

    Create 03-product-list-quiz.py

---
+++ Lesson 12 - Problem Set/03-product-list-quiz.py
@@ -0,0 +1,24 @@
+# Define a procedure, product_list,
+# that takes as input a list of numbers,
+# and returns a number that is
+# the result of multiplying all
+# those numbers together.
+
+
+def product_list(list_of_numbers):
+    i = 0
+    prod = 1
+    while i < len(list_of_numbers):
+        prod = prod * list_of_numbers[i]
+        i += 1
+    return prod
+
+
+print(product_list([9]))
+# >>> 9
+
+print(product_list([1, 2, 3, 4]))
+# >>> 24
+
+print(product_list([]))
+# >>> 1
