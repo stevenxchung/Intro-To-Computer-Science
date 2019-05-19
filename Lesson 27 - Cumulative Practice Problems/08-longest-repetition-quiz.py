87515518f9d7a7930dfd4d6333b01156e6caa8f4
stevenxchung <stevenxchung@gmail.com>
2019-05-19 09:18:00 -0400

    Create 08-longest-repetition-quiz.py

---
+++ Lesson 27 - Cumulative Practice Problems/08-longest-repetition-quiz.py
@@ -0,0 +1,43 @@
+# Question 8: Longest Repetition
+
+# Define a procedure, longest_repetition, that takes as input a
+# list, and returns the element in the list that has the most
+# consecutive repetitions. If there are multiple elements that
+# have the same number of longest repetitions, the result should
+# be the one that appears first. If the input list is empty,
+# it should return None.
+
+
+def longest_repetition(inputList):
+    if len(inputList) > 0:
+        previous = inputList[0]
+        current = inputList[0]
+        most, count = 0, 0
+        for x in inputList:
+            if x == previous:
+                count += 1
+            else:
+                if count > most:
+                    most = count
+                    current = previous
+                previous = x
+                count = 1
+
+        if count > most:
+            current = x
+        return current
+
+
+# For example,
+
+print(longest_repetition([1, 2, 2, 3, 3, 3, 2, 2, 1]))
+# 3
+
+print(longest_repetition(['a', 'b', 'b', 'b', 'c', 'd', 'd', 'd']))
+# b
+
+print(longest_repetition([1, 2, 3, 4, 5]))
+# 1
+
+print(longest_repetition([]))
+# None
